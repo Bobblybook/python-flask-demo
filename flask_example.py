9914e67be520439c6ca74794e5833c2813820b96
Adam <adam@virtutel.com.au>
2023-07-21 20:09:17 +1000

    Plotly graphing on Flask

--- flask_example.py
+++ flask_example.py
@@ -1,22 +1,36 @@
+#!venv/bin/python
+
 from flask import Flask, render_template
+import plotly.express as px
+from json import dumps
+from plotly import utils
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def root_path():
+    return render_template('index.html')
+
+
+@app.route('/greenhouse')
+def greenhouse():
     # Connect to sqlite db
     # grab data from db
     # generate plotly graph
     # export plotly graph to javascript
-    posts_python = 'some code eg. json graph'
-    return render_template('index.html', posts_html=posts_python)
+
+    df = px.data.iris()
+    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+                     size='petal_length', hover_data=['petal_width'])
+    # fig.show()
+    fig_json = dumps(fig, cls=utils.PlotlyJSONEncoder)
+
+    return render_template('greenhouse.html', graph_JSON=fig_json)
 
 
-@app.route('/custom_page')
-def page1():
-    return 'Page 1'
-
-
+# Start the web server, only if this script is run directly.
+# In a production environment this would not be run this way and the
+# Flask code would be hosted using an external WRGI server for stability/performance.
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, True)
+    app.run('0.0.0.0', 8080, True)
